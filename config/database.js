abc2e132d419b3dd4aa371bf96df8a8cde6a8b59
Iksan nursalim <iksannursalim12345@gmail.com>
2023-06-27 06:50:13 +0700

    nn

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const koneksi = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLNAME,
+    database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
     multipleStatements: true
 });
